8208631856bac971782caecd05e061d32befcf97
tcrvision <81814154+tcrvision@users.noreply.github.com>
2021-05-30 17:52:47 +0800

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/TCR_Windmill.dir/DependInfo.cmake
@@ -0,0 +1,36 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/czh/CLionProjects/TCR-Windmill/armor/src/Armor_acquire.cpp" "/home/czh/CLionProjects/TCR-Windmill/cmake-build-debug/CMakeFiles/TCR_Windmill.dir/armor/src/Armor_acquire.cpp.o"
+  "/home/czh/CLionProjects/TCR-Windmill/buff/src/buff_detect.cpp" "/home/czh/CLionProjects/TCR-Windmill/cmake-build-debug/CMakeFiles/TCR_Windmill.dir/buff/src/buff_detect.cpp.o"
+  "/home/czh/CLionProjects/TCR-Windmill/buff/src/solve_angle.cpp" "/home/czh/CLionProjects/TCR-Windmill/cmake-build-debug/CMakeFiles/TCR_Windmill.dir/buff/src/solve_angle.cpp.o"
+  "/home/czh/CLionProjects/TCR-Windmill/main.cpp" "/home/czh/CLionProjects/TCR-Windmill/cmake-build-debug/CMakeFiles/TCR_Windmill.dir/main.cpp.o"
+  "/home/czh/CLionProjects/TCR-Windmill/others/src/additions.cpp" "/home/czh/CLionProjects/TCR-Windmill/cmake-build-debug/CMakeFiles/TCR_Windmill.dir/others/src/additions.cpp.o"
+  "/home/czh/CLionProjects/TCR-Windmill/others/src/camera_device.cpp" "/home/czh/CLionProjects/TCR-Windmill/cmake-build-debug/CMakeFiles/TCR_Windmill.dir/others/src/camera_device.cpp.o"
+  "/home/czh/CLionProjects/TCR-Windmill/others/src/camera_wrapper.cpp" "/home/czh/CLionProjects/TCR-Windmill/cmake-build-debug/CMakeFiles/TCR_Windmill.dir/others/src/camera_wrapper.cpp.o"
+  "/home/czh/CLionProjects/TCR-Windmill/others/src/save_video.cpp" "/home/czh/CLionProjects/TCR-Windmill/cmake-build-debug/CMakeFiles/TCR_Windmill.dir/others/src/save_video.cpp.o"
+  "/home/czh/CLionProjects/TCR-Windmill/others/src/serialport.cpp" "/home/czh/CLionProjects/TCR-Windmill/cmake-build-debug/CMakeFiles/TCR_Windmill.dir/others/src/serialport.cpp.o"
+  "/home/czh/CLionProjects/TCR-Windmill/others/src/systime.cpp" "/home/czh/CLionProjects/TCR-Windmill/cmake-build-debug/CMakeFiles/TCR_Windmill.dir/others/src/systime.cpp.o"
+  "/home/czh/CLionProjects/TCR-Windmill/others/src/thread_control.cpp" "/home/czh/CLionProjects/TCR-Windmill/cmake-build-debug/CMakeFiles/TCR_Windmill.dir/others/src/thread_control.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../buff/include"
+  "../armor/include"
+  "../others/include"
+  "../usr/lib/libgxiapi.so"
+  "/usr/local/opencv340/include"
+  "/usr/local/opencv340/include/opencv"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
