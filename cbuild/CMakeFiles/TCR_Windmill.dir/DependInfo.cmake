19ec4b3c095e3de6be7aabf21ce1a246ab829879
tcrvision <81814154+tcrvision@users.noreply.github.com>
2021-05-30 17:55:13 +0800

    Add files via upload

---
+++ cbuild/CMakeFiles/TCR_Windmill.dir/DependInfo.cmake
@@ -0,0 +1,36 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/tkkrobot/share/TCR-Windmill/armor/src/Armor_acquire.cpp" "/home/tkkrobot/share/TCR-Windmill/cbuild/CMakeFiles/TCR_Windmill.dir/armor/src/Armor_acquire.cpp.o"
+  "/home/tkkrobot/share/TCR-Windmill/buff/src/buff_detect.cpp" "/home/tkkrobot/share/TCR-Windmill/cbuild/CMakeFiles/TCR_Windmill.dir/buff/src/buff_detect.cpp.o"
+  "/home/tkkrobot/share/TCR-Windmill/buff/src/solve_angle.cpp" "/home/tkkrobot/share/TCR-Windmill/cbuild/CMakeFiles/TCR_Windmill.dir/buff/src/solve_angle.cpp.o"
+  "/home/tkkrobot/share/TCR-Windmill/main.cpp" "/home/tkkrobot/share/TCR-Windmill/cbuild/CMakeFiles/TCR_Windmill.dir/main.cpp.o"
+  "/home/tkkrobot/share/TCR-Windmill/others/src/additions.cpp" "/home/tkkrobot/share/TCR-Windmill/cbuild/CMakeFiles/TCR_Windmill.dir/others/src/additions.cpp.o"
+  "/home/tkkrobot/share/TCR-Windmill/others/src/camera_device.cpp" "/home/tkkrobot/share/TCR-Windmill/cbuild/CMakeFiles/TCR_Windmill.dir/others/src/camera_device.cpp.o"
+  "/home/tkkrobot/share/TCR-Windmill/others/src/camera_wrapper.cpp" "/home/tkkrobot/share/TCR-Windmill/cbuild/CMakeFiles/TCR_Windmill.dir/others/src/camera_wrapper.cpp.o"
+  "/home/tkkrobot/share/TCR-Windmill/others/src/save_video.cpp" "/home/tkkrobot/share/TCR-Windmill/cbuild/CMakeFiles/TCR_Windmill.dir/others/src/save_video.cpp.o"
+  "/home/tkkrobot/share/TCR-Windmill/others/src/serialport.cpp" "/home/tkkrobot/share/TCR-Windmill/cbuild/CMakeFiles/TCR_Windmill.dir/others/src/serialport.cpp.o"
+  "/home/tkkrobot/share/TCR-Windmill/others/src/systime.cpp" "/home/tkkrobot/share/TCR-Windmill/cbuild/CMakeFiles/TCR_Windmill.dir/others/src/systime.cpp.o"
+  "/home/tkkrobot/share/TCR-Windmill/others/src/thread_control.cpp" "/home/tkkrobot/share/TCR-Windmill/cbuild/CMakeFiles/TCR_Windmill.dir/others/src/thread_control.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../buff/include"
+  "../armor/include"
+  "../others/include"
+  "../usr/lib/libgxiapi.so"
+  "/opt/ros/kinetic/include/opencv-3.3.1-dev"
+  "/opt/ros/kinetic/include/opencv-3.3.1-dev/opencv"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cbuild/CMakeFiles/TCR_Windmill.dir/cmake_clean.cmake
@@ -0,0 +1,20 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TCR_Windmill.dir/main.cpp.o"
+  "CMakeFiles/TCR_Windmill.dir/armor/src/Armor_acquire.cpp.o"
+  "CMakeFiles/TCR_Windmill.dir/others/src/save_video.cpp.o"
+  "CMakeFiles/TCR_Windmill.dir/others/src/additions.cpp.o"
+  "CMakeFiles/TCR_Windmill.dir/others/src/camera_device.cpp.o"
+  "CMakeFiles/TCR_Windmill.dir/others/src/thread_control.cpp.o"
+  "CMakeFiles/TCR_Windmill.dir/others/src/systime.cpp.o"
+  "CMakeFiles/TCR_Windmill.dir/others/src/camera_wrapper.cpp.o"
+  "CMakeFiles/TCR_Windmill.dir/others/src/serialport.cpp.o"
+  "CMakeFiles/TCR_Windmill.dir/buff/src/buff_detect.cpp.o"
+  "CMakeFiles/TCR_Windmill.dir/buff/src/solve_angle.cpp.o"
+  "TCR_Windmill.pdb"
+  "TCR_Windmill"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TCR_Windmill.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
